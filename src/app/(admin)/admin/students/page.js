360ab29bb7d902abbc1492639c80006a0c2b53a1
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-10-28 07:16:56 -0700

    trainertable and trainer modal ready

--- src/app/(admin)/admin/students/page.js
+++ src/app/(admin)/admin/students/page.js
@@ -1,18 +1,23 @@
-import { StudentForm } from "@/components/Data Modals/studentModal"
-import { StudentTable } from "@/components/Data Tables/studentTable"
+import { StudentForm } from "@/components/Data Modals/studentModal";
+import { StudentTable } from "@/components/Data Tables/studentTable";
 
-function Students (){
-    return(
-        <div className="container mx-auto p-10">
-            <div className="flex justify-between">
-            <h1 className="text-center font-bold text-2xl" style={{
-                fontFamily:'poppins'
-            }}>Students</h1>
-            <StudentForm />
-            </div>
-           <StudentTable />
-        </div>
-    )
+function Students() {
+  return (
+    <div className="container mx-auto p-10">
+      <div className="flex justify-between">
+        <h1
+          className="text-center font-bold text-2xl"
+          style={{
+            fontFamily: "poppins",
+          }}
+        >
+          Students
+        </h1>
+        <StudentForm />
+      </div>
+      <StudentTable />
+    </div>
+  );
 }
 
-export default Students
+export default Students;
--- src/app/(admin)/admin/trainers/page.js
+++ src/app/(admin)/admin/trainers/page.js
@@ -1,5 +1,23 @@
+import { TrainerForm } from "@/components/Data Modals/trainerModal";
+import { TrainersTable } from "@/components/Data Tables/trainersTable";
+
 function Trainers() {
-  return <h1 className="text-center font-bold text-2xl">Trainers</h1>;
+  return (
+    <div className="container mx-auto p-10">
+      <div className="flex justify-between">
+        <h1
+          className="text-center font-bold text-2xl"
+          style={{
+            fontFamily: "poppins",
+          }}
+        >
+          Trainers
+        </h1>
+        <TrainerForm />
+      </div>
+      <TrainersTable />
+    </div>
+  );
 }
 
 export default Trainers;
