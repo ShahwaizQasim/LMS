655c78aab49ffa0ab4cdb9e60f5c8db66a2d8b94
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-03 06:14:37 -0800

    login route done

---
+++ src/app/api/users/login/route.js
@@ -0,0 +1,40 @@
+import { ConnectDB } from "@/lib/dbConnect";
+import { UserModal } from "@/lib/modals/userModal";
+import bcrypt from "bcrypt";
+import { jwt } from "jsonwebtoken";
+
+export async function POST(request) {
+  await ConnectDB();
+  const obj = await request.json();
+
+  const user = await UserModal.findOne({ email: obj.email });
+  if (!user)
+    return Response.json(
+      { error: true, msg: "User Not Found" },
+      {
+        status: 404,
+      }
+    );
+
+  const isPasswordMatch = await bcrypt.compare(obj.password, user.password);
+  if (!isPasswordMatch)
+    return Response.json(
+      { error: true, msg: "Password is not match" },
+      { status: 400 }
+    );
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_KEY
+  );
+
+  return Response.json(
+    {
+      error: false,
+      msg: "Login Successfully",
+      user,
+      token,
+    },
+    { status: 200 }
+  );
+}
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,11 +1,12 @@
 import { ConnectDB } from "@/lib/dbConnect";
 import { UserModal } from "@/lib/modals/userModal";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export async function POST(request) {
   await ConnectDB();
   const obj = await request.json();
-  console.log("obj=>", obj);
+  // console.log("obj=>", obj);
 
   const user = await UserModal.findOne({ email: obj.email });
   console.log("user->", user);
@@ -17,14 +18,23 @@ export async function POST(request) {
     );
 
   const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(obj.email, saltRounds);
-  let password = obj.password; // simple password
-  password = hashedPassword; // simple password ko hashed me convert kiya hai
+  const hashedPassword = await bcrypt.hash(obj.password, saltRounds);
+  obj.password = hashedPassword; // simple password ko hashed me convert kiya hai
 
-  let newUser = new UserModal({ ...obj });
-  newUser = newUser.save();
+  let newUser = await new UserModal({ ...obj });
+  newUser = await newUser.save();
 
-  return Response.json("User Post successfully");
+  const token = jwt.sign(
+    { id: newUser._id, role: newUser.role },
+    process.env.JWT_KEY
+  );
+
+  return Response.json({
+    error: "false",
+    msg: "User Added Successfully",
+    user: newUser,
+    token,
+  });
 }
 
 export async function GET(request) {
