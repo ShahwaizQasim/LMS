8eb6a76a2500788ba6ebb832b3fafee05e7d5bcc
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-03 22:50:09 -0800

    code formatted

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -3,30 +3,30 @@ import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
-      <Tabs defaultValue="Dashboard"  className="w-full">
-        <TabsList className="w-full">
-          <Link href={"/admin/dashboard"}>
-            <TabsTrigger value="Dashboard">Dashboard</TabsTrigger>
-          </Link>
-          <Link href={"/admin/courses"}>
-            <TabsTrigger value="Courses">Courses</TabsTrigger>
-          </Link>
-          <Link href={"/admin/batches"}>
-            <TabsTrigger value="Batches">Batches</TabsTrigger>
-          </Link>
-          <Link href={"/admin/trainers"}>
-            <TabsTrigger value="Trainers">Trainers</TabsTrigger>
-          </Link>
-          <Link href={"/admin/students"}>
-            <TabsTrigger value="Students">Students</TabsTrigger>
-          </Link>
-        </TabsList>
-        {/* <TabsContent value="Admin">{children}</TabsContent> */}
-        <TabsContent value="Dashboard">{children}</TabsContent>
-        <TabsContent value="Courses">{children}</TabsContent>
-        <TabsContent value="Batches">{children}</TabsContent>
-        <TabsContent value="Trainers">{children}</TabsContent>
-        <TabsContent value="Students">{children}</TabsContent>
-      </Tabs>
+    <Tabs defaultValue="Dashboard" className="w-full">
+      <TabsList className="w-full">
+        <Link href={"/admin/dashboard"}>
+          <TabsTrigger value="Dashboard">Dashboard</TabsTrigger>
+        </Link>
+        <Link href={"/admin/courses"}>
+          <TabsTrigger value="Courses">Courses</TabsTrigger>
+        </Link>
+        <Link href={"/admin/batches"}>
+          <TabsTrigger value="Batches">Batches</TabsTrigger>
+        </Link>
+        <Link href={"/admin/trainers"}>
+          <TabsTrigger value="Trainers">Trainers</TabsTrigger>
+        </Link>
+        <Link href={"/admin/students"}>
+          <TabsTrigger value="Students">Students</TabsTrigger>
+        </Link>
+      </TabsList>
+      {/* <TabsContent value="Admin">{children}</TabsContent> */}
+      <TabsContent value="Dashboard">{children}</TabsContent>
+      <TabsContent value="Courses">{children}</TabsContent>
+      <TabsContent value="Batches">{children}</TabsContent>
+      <TabsContent value="Trainers">{children}</TabsContent>
+      <TabsContent value="Students">{children}</TabsContent>
+    </Tabs>
   );
 }
